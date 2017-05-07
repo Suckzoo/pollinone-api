a99c489b94e0c722e98ffd49e12518a993db9d4a
Seokju Hong <tjkj555@gmail.com>
2017-05-08 02:55:28 +0900

    Implements main logic (not tested)

--- common/models/member.js
+++ common/models/member.js
@@ -1,5 +1,87 @@
 'use strict';
 
 module.exports = function(Member) {
+  Member.vote = (id, credential, cb) => {
+    Member.findOne({
+      where: {
+        voteId: id,
+        credential,
+      },
+      include: 'vote',
+    }).then(member => {
+      if (!member) {
+        throw 'invalid membership';
+      }
+      member = member.toJSON();
+      let vote = member.vote;
+      if (vote.status !== 'start') {
+        throw 'currently not voting';
+      }
+      let item = vote.currentItem;
+      member.item = item;
+      return Member.upsert(member);
+    }).then(member => {
+      cb(null, {
+        item: member.item,
+        credential: member.credential,
+      });
+    }).catch(err => {
+      cb(err);
+    });
+  };
 
+  Member.remoteMethod('vote', {
+    accepts: [{
+      arg: 'id',
+      type: 'number',
+      required: true,
+    }, {
+      arg: 'credential',
+      type: 'string',
+      required: true,
+    }],
+    returns: {
+      root: true,
+    },
+    http: {
+      verb: 'get',
+      path: '/vote/:id',
+    },
+  });
+
+  Member.withdraw = (id, credential, cb) => {
+    Member.findOne({
+      voteId: id,
+      credential,
+    }).then(member => {
+      if (!member) {
+        throw 'no membership found';
+      }
+      member.item = null;
+      return Member.upsert(member);
+    }).then(() => {
+      cb(null, {success: true});
+    }).catch(err => {
+      cb(err);
+    });
+  };
+
+  Member.remoteMethod('withdraw', {
+    accepts: [{
+      arg: 'id',
+      type: 'number',
+      required: true,
+    }, {
+      arg: 'credential',
+      type: 'string',
+      required: true,
+    }],
+    returns: {
+      root: true,
+    },
+    http: {
+      verb: 'delete',
+      path: '/vote/:id',
+    },
+  })
 };
--- common/models/vote.js
+++ common/models/vote.js
@@ -1,17 +1,17 @@
 'use strict';
 const randomstring = require('randomstring');
+const moment = require('moment');
 
 module.exports = function(Vote) {
-  Vote.createVote = cb => {
-    let code, key, rootCredential;
-    code = randomstring.generate({
-      length: 6,
-      charset: 'numeric',
-    });
+  Vote.createVote = (title, itemCount, cb) => {
+    let key, rootCredential;
     key = randomstring.generate(20);
     rootCredential = randomstring.generate(10);
-    Vote.create({
-      code,
+    title = title || `Vote created at ${moment().format('YYYY-MM-DD HH:mm')}`;
+    itemCount = itemCount || 2;
+    return Vote.create({
+      title,
+      itemCount,
       key,
       rootCredential,
       status: 'open',
@@ -23,35 +23,27 @@ module.exports = function(Vote) {
   };
 
   Vote.remoteMethod('createVote', {
-    returns: [{
-      arg: 'code',
+    accepts: [{
+      arg: 'title',
       type: 'string',
     }, {
-      arg: 'key',
-      type: 'string',
-    }, {
-      arg: 'rootCredential',
-      type: 'string',
-    }, {
-      arg: 'status',
-      type: 'string',
+      arg: 'itemCount',
+      type: 'number',
     }],
+    returns: {
+      root: true,
+    },
     http: {
       verb: 'post',
     },
   });
 
-  Vote.start = (code, rootCredential, cb) => {
-    Vote.findOne({
-      where: {
-        code,
-        rootCredential,
-      },
-    }).then(vote => {
-      if (!vote) {
-        throw 'no vote room available';
+  Vote.start = (id, rootCredential, cb) => {
+    _findVoteAsRoot(id, rootCredential).then(vote => {
+      if (vote.status !== 'init') {
+        throw 'already started vote';
       }
-      vote.status = 'start';
+      vote.status = 'voting';
       return Vote.upsert(vote);
     }).then(vote => {
       cb(null, vote);
@@ -62,32 +54,155 @@ module.exports = function(Vote) {
 
   Vote.remoteMethod('start', {
     accepts: [{
-      arg: 'code',
-      type: 'string',
+      arg: 'id',
+      type: 'number',
+      required: true,
     }, {
       arg: 'rootCredential',
       type: 'string',
+      required: true,
     }],
+    returns: {
+      root: true
+    },
     http: {
       verb: 'get',
+      path: '/start/:id',
     },
   });
 
-  Vote.join = cb => {
-    
+  Vote.join = (id, key, cb) => {
+    Vote.findOne({
+      where: {
+        id,
+        key,
+      },
+    }).then(vote => {
+      if (!vote) {
+        throw 'no vote room available';
+      }
+      if (vote.status !== 'init') {
+        throw 'user is not allowed to join this vote';
+      }
+      return Vote.app.models.Member.create({
+        credential: randomstring.generate(20),
+        item: null,
+        vote,
+      });
+    }).then(member => {
+      cb(null, member);
+    }).catch(err => {
+      cb(err);
+    });
   };
 
   Vote.remoteMethod('join', {
-
-  });
-
-  Vote.count = cb => {
-
-  };
-
-  Vote.remoteMethod('count', {
+    accepts: [{
+      arg: 'id',
+      type: 'number',
+    }, {
+      arg: 'key',
+      type: 'string',
+    }],
+    returns: {
+      root: true,
+    },
     http: {
       verb: 'get',
+      path: '/join/:id',
     },
   });
+
+  Vote.collect = (id, rootCredential, cb) => {
+    _findVoteAsRoot(id, rootCredential).then(vote => {
+      vote.currentItem++;
+      if (vote.currentItem === vote.itemCount) {
+        vote.status = 'finished';
+      }
+      return Vote.upsert(vote);
+    }).then(vote => {
+      cb(null, vote);
+    }).catch(err => {
+      cb(err);
+    });
+  };
+
+  Vote.remoteMethod('collect', {
+    accepts: [{
+      arg: 'id',
+      type: 'number',
+      required: true,
+    }, {
+      arg: 'rootCredential',
+      type: 'string',
+      required: true,
+    }],
+    returns: {
+      root: true,
+    },
+    http: {
+      verb: 'get',
+      path: '/collect/:id',
+    },
+  });
+
+  Vote.close = (id, rootCredential, cb) => {
+    let result = [];
+    let vote = null;
+    _findVoteAsRoot(id, rootCredential).then(_vote => {
+      vote = _vote;
+      if (vote.status !== 'finished') {
+        throw 'vote not finished';
+      }
+      return Promise.all(Array.from(Array(vote.itemCount).keys()).map(item => (
+        Vote.app.models.Member.count({
+          where: {
+            voteId: id,
+            item: item,
+          },
+        }))
+      ));
+    }).then(_result => {
+      result = _result;
+      vote.status = 'closed';
+      return Vote.upsert(vote);
+    }).then(() => {
+      cb(null, result);
+    }).catch(err => {
+      cb(err);
+    });
+  };
+
+  Vote.remoteMethod('close', {
+    accepts: [{
+      arg: 'id',
+      type: 'number',
+      required: true,
+    }, {
+      arg: 'rootCredential',
+      type: 'string',
+      required: true,
+    }],
+    returns: {
+      root: true,
+    },
+    http: {
+      verb: 'get',
+      path: '/close/:id',
+    },
+  });
+
+  function _findVoteAsRoot(id, rootCredential) {
+    return Vote.findOne({
+      where: {
+        id,
+        rootCredential,
+      },
+    }).then(vote => {
+      if (!vote) {
+        throw 'vote instance not found';
+      }
+      return vote;
+    });
+  }
 };
